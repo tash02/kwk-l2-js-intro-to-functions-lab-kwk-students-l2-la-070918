604b6eecf62bc1f71ce609d0fbfd43a837cdbaa4
tash02 <tash.brinkley@gmail.com>
2018-07-13 22:03:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 // Write your code in this file!
+function getUKTime () {
+return new Date().getHours() + 8
+}
 
+function discountTicket (price){
+  let ticketprice = price * 0.8 
+  return ticketprice
+}
+
+function tweetReply (string){
+  return `Hey ${string}, thanks so much for the support! I really appreciate it!`
+}
 
